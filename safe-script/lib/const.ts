7fc3636ec8ab7949cef3dd6110e7ee73bb219b07
Dmitriy Babenko <159453675+dmitriy-woof-software@users.noreply.github.com>
2025-03-07 11:52:58 -0600

    Update const.ts

--- safe-script/lib/const.ts
+++ safe-script/lib/const.ts
@@ -11,6 +11,7 @@ export const KING_SAFE_WALLET = "0xF46D3734564ef9a5a16fC3B1216831a28f78e2B5";
 export const KING_PROTOCOL_PROXY = "0x8F08B70456eb22f6109F57b8fafE862ED28E6040";
 export const KING_PROTOCOL_CORE_IMPL =
   "0x1cB489ef513E1Cc35C4657c91853A2E6fF1957dE";
+// This contract was used to update LRT2 name to KING. Please, don't use this contact anywhere else 
 export const KING_PROTOCOL_DUMMY_IMPL =
   "0x8E029cEDC7Daf4d9cFFe56AC6771dE266F3CCAdc";
 
